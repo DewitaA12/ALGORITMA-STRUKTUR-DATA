551918a99f4ffad53d3aa106d80a895b71c1d475
DewitaA12 <xsclthzn@gmail.com>
2024-03-06 14:10:09 +0700

    _

--- Mobil07.java
+++ Mobil07.java
@@ -1,37 +1,33 @@
-import java.util.Scanner;
+import java.util.ArrayList;
 
-public class Mobil07{
+class Mobil07{
     String nama;
     public int c2;
     public int hargaBeli;
-    public static int thnProduksi;
+    public int thnProduksi;
 
 public Mobil07(String nm, int cc, int hb, int tp){
-    nama = nm;
-    c2 = cc;
-    hargaBeli = hb;
-    thnProduksi = tp;
+        this.nama = nm;
+        this.c2 = cc;
+        this.hargaBeli = hb;
+        this.thnProduksi = tp;
 
 }
     
-    public double hitungPajak(int c2){
-        double pajak;
+    public double hitungPajak(){
         if ( c2 > 1500) {
-            pajak =  0.10*hargaBeli;
+            return hargaBeli*0.10;
         }else {
-            pajak = 0.75*hargaBeli;
+            return hargaBeli*0.075;
         }
-        return pajak;
     }
 
-    public double hitungBiayaPerawatan(int thnProduksi){
-        double biayaPerawatan;
+    public double hitungBiayaPerawatan(){
         if (thnProduksi < 2010){
-            biayaPerawatan = 0.8*hargaBeli;
+            return hargaBeli*0.08;
         }else {
-            biayaPerawatan = 0.5*hargaBeli;
+            return hargaBeli*0.05;
         }
-        return biayaPerawatan;
     }
 
     public double hitungBiayaTotal() {
@@ -40,56 +36,54 @@ public Mobil07(String nm, int cc, int hb, int tp){
         return pajak + biayaPerawatan;
     }
 
-    public void printData(){
-        System.out.println("Pajak Mobil ke-" +n+ " = Rp. " + mbArray[n].hitungPajak());
-        System.out.println("Biaya Perawatan Mobil ke-" +n+ " = Rp. " + mbArray[n].hitungBiayaPerawatan());
-        System.out.println("Biaya Total Mobil ke-" +n+ "= Rp. " + mbArray[n].hitungBiayaTotal());
+    public int cc(){
+        return c2;
     }
-    
-    public static void main(String[] args) {
-        Mobil07[] mbArray = new Mobil07[n];
-        Scanner sc7 = new Scanner(System.in);
-        for (int n = 0; n < mbArray.length; n++) {
-            System.out.print("Nama Mobil = ");
-            String nama = sc7.nextLine();
-            System.out.print("CC Mobil = ");
-            int c2 = sc7.nextInt();
-            System.out.print("Harga Beli = ");
-            int hargaBeli = sc7.nextInt();
-            System.out.print("Tahun Produksi = ");
-            int thnProduksi = sc7.nextInt();
-            sc7.nextLine(); 
 
-            mbArray[n] = new Mobil07(nama, c2, hargaBeli, thnProduksi);
-        }
+    public int thnProduksi(){
+        return thnProduksi;
+    }
+
+
+    public String printData(){
+         return "Merk: " + nama + ", Harga Beli: " + hargaBeli + ", CC: " + c2 + ", Tahun Produksi: " + thnProduksi;
+    }
+}
+public class Mobil07Main {
+    public static void main(String[] args) {
+        ArrayList<Mobil07> mobilList = new ArrayList<>();
+        mobilList.add(new Mobil07("MCLAREN", 2000, 30000, 2024));
+        mobilList.add(new Mobil07("TOYOTA", 1500, 8000, 2014));
+        mobilList.add(new Mobil07("CHERRY", 1200, 5000, 2004));
 
         double totalBiayaPerawatan = 0;
         int mobilThnLebih2010 = 0;
-        for (int n = 0; n < mbArray.length; n++) {
-            if (Mobil07.thnProduksi > 2010) {
-                totalBiayaPerawatan += mbArray[n].hitungBiayaPerawatan();
+        for (Mobil07 mobil : mobilList) {
+            if (mobil.thnProduksi() > 2010) {
+                totalBiayaPerawatan += mobil.hitungBiayaPerawatan();
                 mobilThnLebih2010++;
             }
         }
+
         double rataBiayaPerawatan = totalBiayaPerawatan / mobilThnLebih2010;
         System.out.println("Rata-rata biaya perawatan mobil tahun produksi > 2010: " + rataBiayaPerawatan);
 
-        double maxBiayaTotal = mbArray[0].hitungBiayaTotal();
-        int maxBiayaTotalIdx = 0;
-        for (int n = 1; n < mbArray.length; n++) {
-            if (mbArray[n].hitungBiayaTotal() > maxBiayaTotal) {
-                maxBiayaTotal = mbArray[n].hitungBiayaTotal();
-                maxBiayaTotalIdx = n;
+        double maxBiayaTotal = 0;
+        Mobil07 mobilTermahal = null;
+        for (Mobil07 mobil : mobilList) {
+            if (mobil.cc() > 1500 && mobil.thnProduksi() < 2010) {
+                double biayaTotal = mobil.hitungBiayaTotal();
+                if (biayaTotal > maxBiayaTotal) {
+                    maxBiayaTotal = biayaTotal;
+                    mobilTermahal = mobil;
+                }
             }
         }
-        System.out.println("Max Biaya Total = " + maxBiayaTotal);
-            }
+        
+        if (mobilTermahal != null) {
+            System.out.printf("Mobil dengan biaya total termahal:%n%s%n dengan biaya total sebesar %.2f%n", mobilTermahal.printData(), maxBiayaTotal);
+        } else {
+            System.out.println("Tidak ada mobil yang memenuhi kriteria.");
         }
-                
-
-
-    
-        
-        
-        
-   
+    }
+}

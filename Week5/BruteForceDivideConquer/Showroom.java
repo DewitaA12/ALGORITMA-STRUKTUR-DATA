4d8781fede750651f809fea672d9ddb35a3fbc74
DewitaA12 <xsclthzn@gmail.com>
2024-03-16 11:23:26 +0700

    Showroom class

---
+++ Week5/BruteForceDivideConquer/Showroom.java
@@ -0,0 +1,17 @@
+package Week5.BruteForceDivideConquer;
+
+public class Showroom {
+    public String merk, tipe, tahun;
+    int topAcceleration;
+    int  topPower;
+    Showroom sr[] = new Showroom[8];
+
+    public Showroom (String mrk, String typ, String thn, int tA, int tP){
+        this.merk = mrk;
+        this.tipe = typ;
+        this.tahun = thn;
+        this.topAcceleration = tA;
+        this.topPower = tP;
+    }
+
+}

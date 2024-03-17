a4b9f3ee3aa24598c2fb747f3f23196120afa7f1
DewitaA12 <xsclthzn@gmail.com>
2024-03-17 10:30:26 +0700

    add constructor

--- Week5/BruteForceDivideConquer/Faktorial.java
+++ Week5/BruteForceDivideConquer/Faktorial.java
@@ -3,10 +3,25 @@ package Week5.BruteForceDivideConquer;
 public class Faktorial {
    public int nilai;
 
+    public Faktorial(int n, int el){
+        nilai = n;
+   }
+
+// int faktorialBF(int n){
+//     int fakto = 1;
+//     for(int i=1; i<=n; i++){
+//         fakto = fakto*i;
+//     }
+//     return fakto;
+// }
+
+//mengubah method faktorialBF() tidak menggunakan for
 int faktorialBF(int n){
     int fakto = 1;
-    for(int i=1; i<=n; i++){
+    int i = 1;
+    while (i<=n){
         fakto = fakto*i;
+        i++;
     }
     return fakto;
 }

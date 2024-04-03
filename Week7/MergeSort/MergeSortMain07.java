49f72de0d580704bb4ae34ab1ad72be6588666dc
DewitaA12 <xsclthzn@gmail.com>
2024-04-03 13:16:08 +0700

    Merge Sort

---
+++ Week7/MergeSort/MergeSortMain07.java
@@ -0,0 +1,14 @@
+package Week7.MergeSort;
+
+public class MergeSortMain07 {
+    public static void main(String[] args) {
+        int data[] = {10, 40, 30, 50, 70, 20, 100, 90};
+        System.out.println("sorting dengan merge sort");
+        MergeSorting07 mSort = new MergeSorting07();
+        System.out.println("data awal");
+        mSort.printArray(data);
+        mSort.mergeSort(data);
+        System.out.println("setelah diurutkan");
+        mSort.printArray(data);
+    }
+}

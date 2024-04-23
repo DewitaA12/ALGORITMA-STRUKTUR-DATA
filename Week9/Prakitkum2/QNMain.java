d2a8c4b283f1d2dee01d7499714e0423a5dc36b1
DewitaA12 <xsclthzn@gmail.com>
2024-04-23 11:53:02 +0700

    fix

---
+++ Week9/Prakitkum2/QNMain.java
@@ -0,0 +1,63 @@
+package Week9.Prakitkum2;
+import java.util.Scanner;
+
+public class QNMain {
+    
+        public static void menu() {
+            System.out.println("Pilih menu : ");
+            System.out.println("1. Antrian baru");
+            System.out.println("2. Antrian keluar");
+            System.out.println("3. Cek Antrian Terdepan");
+            System.out.println("4. Cek Semua Antrian");
+            System.out.println("---------------------");
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Masukkan kapasitas queue : ");
+            int jumlah = sc.nextInt();
+            QueueNasabah antri = new QueueNasabah(jumlah);
+            int pilih;
+
+            do {
+                menu();
+                System.out.print("Pilihan 1/2/3/4 : ");
+                pilih = sc.nextInt();
+                switch (pilih) {
+                    case 1:
+                    System.out.print("No Rekening : ");
+                    String norek = sc.nextLine();
+                    sc.nextLine();
+                    System.out.print("Nama : ");
+                    String nama = sc.nextLine();
+                    System.out.print("Alamat : ");
+                    String alamat = sc.nextLine();
+                    System.out.print("Umur : ");
+                    int umur = sc.nextInt();
+                    System.out.print("Saldo : ");
+                    double saldo = sc.nextDouble();
+                    Nasabah nb = new Nasabah(norek, nama, alamat, umur, saldo);
+                    sc.nextLine();
+                    antri.Enqueque(nb);
+                    break;
+                    case 2:
+                    Nasabah data = antri.Dequeue();
+                    if (!"".equals(data.norek) && !"".equals(data.nama) && !"".equals(data.alamat)
+                    && data.umur !=0 && data.saldo !=0) {
+                        System.out.println("Antrian yang keluar : " + data.norek + " " + data.nama + " " 
+                        + data.alamat + " " + data.umur + " " + data.saldo);
+
+                        break;
+                    }
+                    case 3:
+                    antri.peek();
+                    break;
+                    case 4:
+                    antri.print();
+                    break;
+                }
+            } while (pilih == 1 || pilih == 2 || pilih == 3 || pilih == 4);
+            sc.close();
+        }
+        }
+    
---
+++ Week9/Prakitkum2/QueueNasabah.java
@@ -0,0 +1,111 @@
+package Week9.Prakitkum2;
+
+public class QueueNasabah {
+        Nasabah[] data;
+        int front;
+        int rear;
+        int size;
+        int max;
+    
+        public QueueNasabah(int n) {
+            max = n;
+            data = new Nasabah[max];
+            size = 0;
+            front = rear = -1;
+        }
+    
+         public boolean IsEmpty() {
+            if (size == 0) {
+                return true;
+            } else {
+                return false;
+            }
+         }
+    
+         public boolean IsFull() {
+            if (size == max) {
+                return true;
+            } else {
+                return false;
+            }
+         }
+    
+         public void peek() {
+            if (!IsEmpty()) {
+                System.out.println("Elemen terdepan : " + data[front].norek + " " + data[front].nama
+                + " " + data[front].alamat + " " + data[front].umur + " " + data[front].saldo);
+            } else {
+                System.out.println("Queue masih kosong");
+            }
+         }
+    
+         public void print() {
+            if (IsEmpty()) {
+                System.out.println("Queue masih kosong");
+            } else {
+                int i = front;
+                while (i != rear) {
+                    System.out.println(data[i].norek + " " + data[i].nama + " " + data[i].alamat + " " +
+                    data[i].umur + " " + data[i].saldo + " ");
+                    i = (i+1) % max;
+                }
+                System.out.println(data[i].norek + " " + data[i].nama + " " + data[i].alamat + " " +
+                    data[i].umur + " " + data[i].saldo + " ");
+                System.out.println("Jumlah elemen = " + size);
+            }
+         }
+    
+         public void clear() {
+            if (!IsEmpty()) {
+                front = rear = -1;
+                size = 0;
+                System.out.println("Queue berhaasil dikosongkan");
+            } else {
+                System.out.println("Queue masih kosong");
+            }
+         }
+    
+         public void Enqueque(Nasabah nb) {
+            if (IsFull()) {
+                System.out.println("Queue sudah penuh (Queue Overflow)");
+                System.exit(1); // Queue overflow exit program
+            } else {
+                if (IsEmpty()) {
+                    front = rear = 0;
+                } else {
+                    if (rear == max - 1) {
+                        rear = 0;
+                    } else {
+                        rear++;
+                    }
+                }
+                data[rear] = nb;
+                size++;
+            }
+         }
+    
+         public Nasabah Dequeue() {
+            Nasabah dt = new Nasabah();
+            if (IsEmpty()) {
+                System.out.println("Queue masih kosong (Queue Underflow)");
+                System.exit(1);
+            } else {
+                dt = data[front] ;
+                size--;
+                if (IsEmpty()) {
+                    front = rear = -1;
+                } else {
+                    if (front == max - 1) {
+                        front = 0;
+                    } else {
+                        front++;
+                    }
+                }
+            }
+            return dt;
+         }
+        
+         
+
+
+}

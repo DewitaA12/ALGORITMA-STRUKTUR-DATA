1d6d7cd24035a81a8423ccb9913d69fda4859bd8
DewitaA12 <xsclthzn@gmail.com>
2024-04-24 15:27:01 +0700

    object

---
+++ Rekening07.java
@@ -0,0 +1,26 @@
+public class Rekening07 {
+    String noRek;
+    String nama;
+    String telefon;
+    String email;
+    double saldo;
+    
+    
+
+    Rekening07 (String noR, String nama, String noHP, String email, double saldo){
+    this.noRek = noR;
+    this.nama = nama;
+    this.telefon = noHP;
+    this.email = email;
+    this.saldo = saldo;
+    }
+
+    Rekening07 () {
+
+    }
+
+    public void tampilAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'tampilAll'");
+    }
+}

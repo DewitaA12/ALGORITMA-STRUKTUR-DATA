a664d0ff69406342b36281bcc59fb33d72fdaa33
DewitaA12 <xsclthzn@gmail.com>
2024-05-09 12:49:00 +0700

    DLL P2

--- Week12/DLLMain.java
+++ Week12/DLLMain.java
@@ -2,6 +2,7 @@ package Week12;
 
 public class DLLMain {
     public static void main(String[] args) throws Exception {
+        //Add data
         DoubleLinkedLists dll = new DoubleLinkedLists();
         dll.print();
         System.out.println("Size : " + dll.size());
@@ -19,6 +20,26 @@ public class DLLMain {
         dll.clear();
         dll.print();
         System.out.println("Size : " + dll.size());
-        
+
+        //Remove data
+        dll.addLast(50);
+        dll.addLast(40);
+        dll.addLast(10);
+        dll.addLast(20);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("----------------------");
+        dll.removeFirst();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("----------------------");
+        dll.removeLast();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("----------------------");
+        dll.remove(1);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+
     }
 }

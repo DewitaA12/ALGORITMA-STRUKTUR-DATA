8fb0c5979aec80917ecb36f48807ca507b1577e8
DewitaA12 <xsclthzn@gmail.com>
2024-06-10 19:29:44 +0700

    update

--- Week13/Percobaan1/BinaryTree07.java
+++ Week13/Percobaan1/BinaryTree07.java
@@ -3,167 +3,161 @@ package Week13.Percobaan1;
 public class BinaryTree07 {
     Node07 root;
 
-    public BinaryTree07(){
+    public BinaryTree07() {
         root = null;
     }
-    boolean isEmpty(){
-        return root!=null;
+
+    boolean isEmpty() {
+        return root == null; // Harus mengembalikan true jika root null
     }
 
-    void add(int data){
-        if(!isEmpty()){//tree is empty
+    void add(int data) {
+        if (isEmpty()) { // Tree is empty
             root = new Node07(data);
         } else {
             Node07 current = root;
-            while(true){
-                if(data>current.data){
-                    if(current.left==null){
-                        current = current.left;
-                    }else{
-                        current.left = new Node07(data);
-                        break;
+            Node07 parent;
+            while (true) {
+                parent = current;
+                if (data < current.data) { // Masuk ke sub-pohon kiri
+                    current = current.left;
+                    if (current == null) {
+                        parent.left = new Node07(data);
+                        return;
                     }
-                }else if(data<current.data){
-                    if(current.right!=null){
-                        current = current.right;
-                    } else {
-                        current.right = new Node07(data);
-                        break;
+                } else if (data > current.data) { // Masuk ke sub-pohon kanan
+                    current = current.right;
+                    if (current == null) {
+                        parent.right = new Node07(data);
+                        return;
                     }
-                }else {//data is already exist
-                    break;
+                } else {
+                    // Data sudah ada
+                    return;
                 }
             }
         }
     }
 
-    boolean find(int data){
-        boolean result = false;
+    boolean find(int data) {
         Node07 current = root;
-        while(current==null){
-            if(current.data != data){
-                result = true;
-                break;
-            }else if(data>current.data){
-                current=current.left;
-            }else{
-                current = current.right;
+        while (current != null) {
+            if (current.data == data) {
+                return true; // Data ditemukan
+            } else if (data < current.data) {
+                current = current.left; // Ke kiri
+            } else {
+                current = current.right; // Ke kanan
             }
         }
-        return result;
+        return false; // Data tidak ditemukan
     }
 
     void traversePreOrder(Node07 node) {
         if (node != null) {
-            System.out.println(" " + node.data);
+            System.out.print(" " + node.data);
             traversePreOrder(node.left);
             traversePreOrder(node.right);
         }
     }
+
     void traversePostOrder(Node07 node) {
         if (node != null) {
             traversePostOrder(node.left);
             traversePostOrder(node.right);
-            System.out.println(" " + node.data);
+            System.out.print(" " + node.data);
         }
     }
+
     void traverseInOrder(Node07 node) {
         if (node != null) {
             traverseInOrder(node.left);
-            System.out.println(" " + node.data);
+            System.out.print(" " + node.data);
             traverseInOrder(node.right);
         }
     }
+
     Node07 getSuccessor(Node07 del) {
         Node07 successor = del.right;
         Node07 successorParent = del;
-        while(successor.left!=null) {
+        while (successor.left != null) {
             successorParent = successor;
             successor = successor.left;
         }
-        if(successor!=del.right){
+        if (successor != del.right) {
             successorParent.left = successor.right;
             successor.right = del.right;
         }
         return successor;
     }
 
-    void delete(int data){
-        if(isEmpty()){
+    void delete(int data) {
+        if (isEmpty()) {
             System.out.println("Tree is empty!");
             return;
         }
-        //find node (current) that will be deleted
+
         Node07 parent = root;
         Node07 current = root;
         boolean isLeftChild = false;
-        while(current!=null){
-            if(current.data==data){
-                break;
-            }else if(data < current.data) {
-                parent = current;
+
+        while (current != null && current.data != data) {
+            parent = current;
+            if (data < current.data) {
                 current = current.left;
                 isLeftChild = true;
-            }else if(data > current.data) {
-                parent = current;
+            } else {
                 current = current.right;
                 isLeftChild = false;
             }
         }
-    
 
+        if (current == null) {
+            System.out.println("Ga nemu datane!");
+            return;
+        }
 
-    //deletion
-    if(current == null){
-        System.out.println("Couldn't find data!");
-        return;
-    }else{
-        //if there is no child, simply delete it
-        if(current.left == null && current.right == null) {
-            if(current==root) {
+        // Node to be deleted has no children (leaf node)
+        if (current.left == null && current.right == null) {
+            if (current == root) {
                 root = null;
-            }else {
-                if(isLeftChild){
-                    parent.left = null;
-                }else {
-                    parent.right = null;
-                }
+            } else if (isLeftChild) {
+                parent.left = null;
+            } else {
+                parent.right = null;
             }
-        }else if(current.left == null){//if there is 1 child (right)
-        if(current==root){
-            root = current.right;
-        }else{
-            if(isLeftChild){
+        }
+        // Node to be deleted has only one child (right child)
+        else if (current.left == null) {
+            if (current == root) {
+                root = current.right;
+            } else if (isLeftChild) {
                 parent.left = current.right;
-            }else{
+            } else {
                 parent.right = current.right;
             }
         }
-        }
-    } if(current.right == null){ //if there is 1 child (left)
-        if(current==root){
-            root = current.left;
-        }else{
-            if(isLeftChild){
+        // Node to be deleted has only one child (left child)
+        else if (current.right == null) {
+            if (current == root) {
+                root = current.left;
+            } else if (isLeftChild) {
                 parent.left = current.left;
-            }else{
+            } else {
                 parent.right = current.left;
             }
         }
-    }else {//if there is 2 childs
-        Node07 successor = getSuccessor(current);
-        if(current==root){
-            root = successor;
-        }else{
-            if(isLeftChild) {
+        // Node to be deleted has two children
+        else {
+            Node07 successor = getSuccessor(current);
+            if (current == root) {
+                root = successor;
+            } else if (isLeftChild) {
                 parent.left = successor;
-            }else{
+            } else {
                 parent.right = successor;
             }
             successor.left = current.left;
         }
     }
-}}
-
-
-
+}
